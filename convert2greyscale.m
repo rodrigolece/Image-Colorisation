a330872c5721b754aac31b487024f139c0a4f1c8
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2017-05-04 09:24:38 +0100

    Removed unnecessary rounding with uint8

--- convert2greyscale.m
+++ convert2greyscale.m
@@ -1,6 +1,8 @@
 function out = convert2greyscale(img)
 
-grey = round( 0.3*img(:,:,1) + 0.11*img(:,:,2) + 0.59*img(:,:,3) );
+% grey = round( 0.3*img(:,:,1) + 0.11*img(:,:,2) + 0.59*img(:,:,3) );
+% round is not necessary because multiplication and addition preserve uint8
+grey = 0.3*img(:,:,1) + 0.11*img(:,:,2) + 0.59*img(:,:,3);
 
 % Matlab loads images in uint8 format. imshow only works correctly if this
 % is preserved so the line below cannot be used.
